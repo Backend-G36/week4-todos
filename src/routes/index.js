95bd11e4b46eb6013d833d83f2026b8b5043070a
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2024-06-14 00:28:30 -0300

    advances

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const routerUser = require('./user.router');
+const routerToDo = require('./toDo.router');
+const { verifyJwt } = require('../utils/verifyJWT');
 const router = express.Router();
 
 // colocar las rutas aquí
 router.use('/users', routerUser)
+router.use('/todos', verifyJwt, routerToDo) //🔐
 
 module.exports = router;
